f1a4bb9837b2f9c674d6d7dd60094e80bf28e271
Jack Chen <redchenjs@live.com>
2020-03-11 18:20:07 +0800

    app: add baudrate log

--- main/app_main.c
+++ main/app_main.c
@@ -34,7 +34,7 @@ int app_main(void)
     ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
     ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BTDM));
 
-    ESP_LOGI(TAG, "initialized, tx: 5, rx: 18, rts: 19, cts: 23");
+    ESP_LOGI(TAG, "%d bps, tx: 5, rx: 18, rts: 19, cts: 23", CONFIG_BT_HCI_UART_BAUDRATE);
 
     return 0;
 }
